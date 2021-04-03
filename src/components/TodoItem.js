52e355def9b8a88155f10cc0f39191cac09ee96a
Bruno Martins <bruno.martins217@gmail.com>
2021-04-03 06:18:35 -0300

    check and uncheck

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import TodoContext from '../context/TodoContext';
 
 function TodoItem({ data }) {
+
 const {checked, setChecked} = useContext(TodoContext);
 function handleChange({target}) {
   if (checked.indexOf(target.value) === -1) {
@@ -10,12 +11,11 @@ function handleChange({target}) {
     setChecked(checked.filter((element)=> element !== target.value))
   }
 }
-
   return (
     <li key={data.id} className="ui-state-default">
       <div className="checkbox">
         <label htmlFor="">
-          <input onChange={({target}) => handleChange({target})} type="checkbox" value={data.text} />
+          <input className={checked.includes(data.text) ? 'done' : 'undone'} onChange={({target}) => handleChange({target})} type="checkbox" value={data.text} />
           {data.text}
         </label>
       </div>
